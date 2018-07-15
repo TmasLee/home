ddbc72fa86ecaeb64ab65c2077a83fe8ff4aac2d
Thomas Lee <Thomas Lee>
2018-07-14 22:39:53 -0400

    Added new project

---
+++ src/Projects/MyWSJ/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class main extends Component {
+  render() {
+    return (
+      <div>
+        <a href='https://sendmebiscuits.github.io/mywsj/'>Click here to visit the site</a>
+      </div>
+    )
+  }
+}
+
+export default main;
+
