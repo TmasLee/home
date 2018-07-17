3c4d31993cf15eec15ed8526292d2b0a2b994a63
sendmebiscuits <tmaslee@gmail.com>
2018-07-16 22:25:09 -0400

    update ec2 instance

--- src/Projects/Macros/macros.js
+++ src/Projects/Macros/macros.js
@@ -4,7 +4,7 @@ class Macros extends Component {
   render() {
     return (
       <div>
-        <a href='http://ec2-34-203-189-85.compute-1.amazonaws.com:8080'>Click here to visit the site</a>
+        <a href='http://ec2-54-174-19-245.compute-1.amazonaws.com:8080'>Click here to visit the site</a>
       </div>
     )
   }
